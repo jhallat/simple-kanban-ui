211291b2a01d065fff75d94132a7b3c95f3b444b
John Hallat <jhallat@gmail.com>
2019-02-01 07:35:46 -0500

    update star input for priorities

--- src/app/shared/components/star-input/star-input.component.ts
+++ src/app/shared/components/star-input/star-input.component.ts
@@ -1,30 +1,51 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-star-input',
   templateUrl: './star-input.component.html',
   styleUrls: ['./star-input.component.scss']
 })
-export class StarInputComponent implements OnInit, OnChanges {
+export class StarInputComponent implements OnInit {
 
   constructor() { }
 
+  private _value: number;
   @Input() maximum;
-  @Input() value;
+  @Input() readOnly = false;
+  @Output() valueAssigned = new EventEmitter<number>();
   stars: boolean[];
 
-  ngOnInit() {
-    console.log(this.maximum);
-    console.log(this.value);
-    this.stars = new Array(this.maximum);
-    for (let i = 0; i < this.stars.length; i++) {
-      this.stars[i] = i < this.value;
+  @Input()
+  set value(value) {
+    if (!value || value < 1) {
+      this._value = 1;
+    } else {
+      this._value = value;
+      this.updateStars();
+      this.valueAssigned.emit(this._value);
     }
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(this.maximum);
-    console.log(this.value);
+  get value() {
+    return this._value;
+  }
+
+  ngOnInit() {
+    if (!this.value || this.value < 1) {
+      this.value = 1;
+    }
+    this.updateStars();
+  }
+
+  select(index: number) {
+    if (!this.readOnly) {
+      this.value = index + 1;
+      this.updateStars();
+    }
+  }
+
+  private updateStars() {
     this.stars = new Array(this.maximum);
     for (let i = 0; i < this.stars.length; i++) {
       this.stars[i] = i < this.value;
