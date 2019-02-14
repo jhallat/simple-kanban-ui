4fc476d78692f0d3fcea3f657d96714bc368f476
John Hallat <jhallat@gmail.com>
2019-02-14 07:45:32 -0500

    inital branch

--- projects/logger/src/lib/logger-factory.service.ts
+++ projects/logger/src/lib/logger-factory.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Logger } from './logger';
 import { ConsoleAppender } from './appenders/console-appender';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,14 @@ export class LoggerFactoryService {
 
   constructor() { }
 
+
+
   createLogger(loggerName: string): Logger {
     // TODO define appenders externally, but use console appender for now
+    if (environment.logging) {
+      
+    }
+
     const appenders = [new ConsoleAppender()];
     // TODO define levels externally, but use DEBUG for now
     const level = 'DEBUG';
