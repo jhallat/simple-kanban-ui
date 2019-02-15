359e9112cfd71f1d5fa209d76ac3d23a6c443b30
John Hallat <jhallat@gmail.com>
2019-02-15 07:42:24 -0500

    added configurable log appenders

--- projects/logger/src/lib/logger-factory.service.ts
+++ projects/logger/src/lib/logger-factory.service.ts
@@ -2,25 +2,38 @@ import { Injectable } from '@angular/core';
 import { Logger } from './logger';
 import { ConsoleAppender } from './appenders/console-appender';
 import { environment } from 'src/environments/environment';
+import { Appender } from './appenders/appender';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggerFactoryService {
 
-  constructor() { }
+  appenders:Appender[] = [];
+  constructor() {
+    if (environment.logging) {
+      const logging = environment.logging;
+      if (logging.appenders) {
+        for (let appender of logging.appenders) {
+          if (appender.toLowerCase() === 'console') {
+            this.appenders.push(new ConsoleAppender());
+          }
+        }
+      }
+      if (logging.loggers) {
+        console.log('length ' + logging.loggers.length);
+        for (let logger of logging.loggers) {
+          console.log(logger);
+        }
+      }
+    }
+   }
 
 
 
   createLogger(loggerName: string): Logger {
-    // TODO define appenders externally, but use console appender for now
-    if (environment.logging) {
-      
-    }
-
-    const appenders = [new ConsoleAppender()];
     // TODO define levels externally, but use DEBUG for now
     const level = 'DEBUG';
-    return new Logger(loggerName, level, appenders);
+    return new Logger(loggerName, level, this.appenders);
   }
 }
